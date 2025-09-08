a629295d583421e98e821b9a2be95dbca4cdffb2
Adomakojohn <johnkwakuadomako6@gmail.com>
2025-09-08 07:32:15 +0000

    assign letter grades

--- grade.dart
+++ grade.dart
@@ -5,6 +5,17 @@ void main() {
 
   for (int i = 0; i < students.length; i++) {
     print('${students[i]} scored ${grades[students[i]]}');
+    if (grades[students[i]]! >= 50) {
+      print('${students[i]} had A');
+    } else if (grades[students[i]]! >= 40 && grades[students[i]]! < 50) {
+      print('${students[i]} had B');
+    } else if (grades[students[i]]! >= 30 && grades[students[i]]! < 40) {
+      print('${students[i]} had C');
+    } else if (grades[students[i]]! >= 20 && grades[students[i]]! < 30) {
+      print('${students[i]} had D');
+    } else {
+      print('${students[i]} failed');
+    }
   }
 }
 
@@ -13,10 +24,6 @@ List<String> students = ['Kofi', 'Ama', 'Kwame'];
 Map<String, int> grades = {students[0]: 20, students[1]: 35, students[2]: 55};
 
 double avgGrade(Map<String, int> grades) {
-  if (grades.isEmpty) {
-    return 0.0;
-  }
-
   int totalGrades = 0;
   for (int gradeValue in grades.values) {
     totalGrades += gradeValue;
